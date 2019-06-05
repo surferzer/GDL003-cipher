641e690b32e58d25906ce51d6de3e768d64849c6
ItzelMB <it.mobec@gmail.com>
2019-06-04 19:36:48 -0500

    Orden de pantallas tras eventos

---
+++ src/index.js
@@ -0,0 +1,26 @@
+
+
+
+//DECLARAR MENSAJE PRINCIPAL
+let mensajePrincipal=document.getElementById("mensajePrincipal");
+let introduceMensaje=document.getElementById("introduceMensaje");
+
+//DECLARAR BOTON CODIFICAR
+let botonCodificar=document.getElementById("botonCodificar");
+
+botonCodificar.addEventListener("click", function() {
+  pagina2.style.display="block"
+  pagina1.style.display="none"
+});
+
+
+
+
+//DECLARAR LOS BOTONES DE COLORES
+let botonRojo=getElementById('botonRojo');
+let botonAmarillo=getElementById('botonAmarillo');
+let botonAzul=getElementById('botonAzul');
+let botonNegro=getElementById('botonNegro');
+
+//FUNCION DE LOS BOTONES DE COLORES
+l
